e790985a36c1ea96136b1878aac498900002ce24
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2022-12-20 10:22:44 +0530

    Add files via upload

---
+++ 67_add_binary.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+        string s = "";
+        
+        int c = 0, i = a.size() - 1, j = b.size() - 1;
+        while(i >= 0 || j >= 0 || c == 1)
+        {
+            c += i >= 0 ? a[i --] - '0' : 0;
+            c += j >= 0 ? b[j --] - '0' : 0;
+            s = char(c % 2 + '0') + s;
+            c /= 2;
+        }
+        
+        return s;
+    }
+};
