864f3ff8501ef05fba2f09fac744d8395eaf8956
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2022-12-27 12:05:10 +0530

    Add files via upload

---
+++ 448_ Disappeared_num.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int len = nums.size();
+        for(int i=0; i<len; i++) {
+            int m = abs(nums[i])-1; // index start from 0
+            nums[m] = nums[m]>0 ? -nums[m] : nums[m];
+        }
+        vector<int> res;
+        for(int i = 0; i<len; i++) {
+            if(nums[i] > 0) res.push_back(i+1);
+        }
+        return res;
+    }
+};
