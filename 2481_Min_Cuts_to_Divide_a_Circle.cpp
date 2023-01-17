0bf2cbcf0993665001a3b7eeb6f03874e09b9c7a
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2023-01-17 13:08:24 +0530

    Add files via upload

---
+++ 2481_Min_Cuts_to_Divide_a_Circle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        if(n==1)
+            return 0;
+        
+        if(n%2==0)
+            return n/2;
+        
+        else
+            return n;
+    }
+};
