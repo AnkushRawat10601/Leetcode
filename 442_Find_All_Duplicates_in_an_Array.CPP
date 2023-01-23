ffc29e3b68e1455055be3765a93cb57e8b420dcb
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2023-01-23 12:30:51 +0530

    Add files via upload

---
+++ 442_Find_All_Duplicates_in_an_Array.CPP
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> v;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                v.push_back(nums[i]);
+            }
+        }
+        return v;
+    }
+};
