d9633b43685c7bde62f158179908f209a48f8045
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2023-01-25 12:24:30 +0530

    Add files via upload

---
+++ 48_rotate_img.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0; i<n; i++)
+            for(int j=i+1; j<n; j++)
+                swap(matrix[i][j], matrix[j][i]);
+
+        for(int i=0, k=n-1; i<k; i++, k--){
+            for(int j=0; j<n; j++){
+                swap(matrix[j][i],matrix[j][k]);
+    }
+};
