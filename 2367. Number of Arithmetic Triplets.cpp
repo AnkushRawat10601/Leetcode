3255dd7daf51741deda7f2735a8a3a6c7f37f86f
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2023-02-16 11:51:11 +0530

    Update 2367. Number of Arithmetic Triplets.cpp

--- 2367. Number of Arithmetic Triplets.cpp
+++ 2367. Number of Arithmetic Triplets.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        
-        int cnt = 0;
-        
-        unordered_map<int,bool> mp;
-        
-        // Mark every elem presence in map.
-        for(int i=0;i<nums.size();i++)
-            mp[nums[i]] = true;
-        
-        
-        // For every element say 'elm' check if there exist both numbers, (elm + diff) and (elm - diff) inside map. If yes then increment cnt
-        for(int i=0;i<nums.size();i++)
-        {
-            if(mp[nums[i]-diff] && mp[nums[i]+diff])
-                cnt++;
+        unordered_map<int, int> mp;
+        int ans = 0;
+        for(auto i: nums){
+            if(mp.find(i-diff) != mp.end() && mp.find(i-2*diff) != mp.end()) ans = ans + mp[i-diff]* mp[i-2*diff];
+            mp[i]++;
         }
-        
-		
-		// Happy return :)
-        return cnt;
+        return ans;
     }
 };
