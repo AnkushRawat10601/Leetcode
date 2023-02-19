2425006399242584283c2c4feae74999f40a28c3
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2023-02-19 11:55:04 +0530

    Add files via upload

---
+++ 103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(!root)return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        int i=0;
+        while(!q.empty()){
+           int sz=q.size();
+           vector<int>v;
+           while(sz--){
+               TreeNode * f=q.front();
+               v.push_back(q.front()->val);
+               q.pop();
+               if(f->left)q.push(f->left);
+               if(f->right)q.push(f->right);
+
+           }
+           if(i++%2)
+           reverse(v.begin(),v.end());
+           ans.push_back(v);
+
+        }
+        return ans;
+    
+    }
+};
