3c71536f6510bc056dba1f75b67eda369f4c995a
Ankush Rawat <101389542+AnkushRawat10601@users.noreply.github.com>
2023-02-22 11:33:05 +0530

    Add files via upload

---
+++ 1011. Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int lo = *max_element(weights.begin(), weights.end());
+        int hi = accumulate(weights.begin(), weights.end(), 0);
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cur = 0, need = 1;
+            for (int w : weights) {
+                if (cur + w > mid) {
+                    cur = 0;
+                    need++;
+                }
+                cur += w;
+            }
+            if (need <= days) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+};
+// please upvote if you liked my approach
